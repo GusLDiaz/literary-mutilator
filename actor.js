6e5a468e5be9d8068eaea0e013da0355b9bf662f
Gus Liakos <Gliakos@cnm.edu>
2018-05-09 10:45:00 -0600

    function add

--- actor.js
+++ actor.js
@@ -1,16 +1,33 @@
-function orangeFuzzy() {
-	document.getElementById("fuzzy").style.color = "orange";
-}
-function anagram() {
-	let cap = document.getElementsByName("Reynolds");
-	console.log(cap.length);
-	cap.forEach()
-}
-function highlight() {
-
-}
-function start(){
-	if
+var pFunFlag = 0;
+// function actionHighlight() {
+// 	var a = document.getElementById("Bronsolino");
+// 	a.onclick
+// }
+function pHighlight() {
+	let pFun= document.querySelector("action");
+	if (pFunFlag=0) {
+	for (var x = 0;x < pFun.length; x++) {
+		pFun[x].setAttribute("style","bg-color: #ffff00");
+	}
+	else
+		for (var x = 0;x < pFun.length; x++) {
+			pFun[x].setAttribute("style","bg-color: #ffff00");
+		}
+// function anagram() {
+// 	let cap = document.getElementsByName("Reynolds");
+// 	console.log(cap.length);
+// 	cap.forEach()
+// }
+// function highlight() {
+//
+// }
+boxElem.addEventListener("mousedown", logEvent);
+boxElem.addEventListener("mouseup", logEvent);
+// function start(){
+// 	if "flag" !== "blue" {
+//
+//
+// 	}
 
 
 }
